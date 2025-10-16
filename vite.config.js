08aaa87ffea321e8b3cc2dcb7c12c9f8c2810850
pandi1718m <soundharapandiyan0@gmail.com>
2025-10-16 14:30:55 +0530

    version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from '@/App.jsx'  
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-createRoot(document.getElementById('root')).render(<App />)
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
