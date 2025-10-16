18d9d1bc8493e64fedf46fc7fe3836ef3270cc1f
pandi1718m <soundharapandiyan0@gmail.com>
2025-10-16 12:49:45 +0530

    update main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default defineConfig({
+  plugins: [react()],
+  base: '/organic/',  
+  build: {
+    outDir: 'docs'  
+  }
+})
